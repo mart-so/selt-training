71b199587c9feaf8ae7a6a9768bb1a95a358f1fd
Martin S <martinsoufer@gmail.com>
2024-07-25 16:41:42 -0300

    Adding api

--- api/test/index.js
+++ api/test/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-    context.log('function processed a request.');
+    context.log('function processed a request');
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
